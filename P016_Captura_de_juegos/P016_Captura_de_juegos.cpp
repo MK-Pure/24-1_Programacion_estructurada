5b693c95ff2afc528fec042bded81c7e632c5bf2
Carlos Enciso <trcokkieshd@gmail.com>
2023-11-05 23:49:10 -0600

    Updated P016
    Updated P016, however the code despite being "correct" is having a hard time taking in user input. will try to correct later.

--- P016_Captura_de_juegos/P016_Captura_de_juegos.cpp
+++ P016_Captura_de_juegos/P016_Captura_de_juegos.cpp
@@ -2,18 +2,27 @@
 // Carlos Enciso.
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-	const int tamano_arreglo = 4;
-	int arreglo[tamano_arreglo];
+    const int tamano_arreglo = 5;
+    int arreglo[tamano_arreglo];
 
-	for (int i = 0; i < tamano_arreglo; i++)
-	{
-		arreglo[i] = i * 2;
-	}
-	std::cout << "Valores en el arreglo: ";
+    for (int i = 0; i < tamano_arreglo; i++) 
+    {
 
-	for (int i = 0; i > tamano_arreglo; i++)
-		std::cout << arreglo[i] << "";
+        std::cout << "Introduce un valor para el elemento " << i << ": ";
+        std::cin >> arreglo[i];
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    }
+        std::cout << "Valores en el arreglo: ";
+
+    for (int i = 0; i < tamano_arreglo; i++) 
+    {
+        
+        std::cout << arreglo[i] << " ";
+        
+    }
 }
